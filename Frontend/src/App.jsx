eb87a428cd7670ed44803a6b285ae5479914e241
Wesley SantAnna <wesley.santanna@semeq.com>
2025-05-29 07:19:51 -0300

    Refatoração do meu App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,72 +1,10 @@
-import { useState, useEffect } from 'react';
-import { login, fetchUserData } from './ApiService';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
-function App() {
-
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [accessToken, setAccessToken] = useState(null);
-  const [userData, setUserData] = useState(null);
-  const [error, setError] = useState(null);
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const tokenData = await login(username, password);
-      setAccessToken(tokenData.access);
-      
-      const userDataResponse = await fetchUserData(tokenData.access); 
-      setUserData(userDataResponse);
-    } catch (err) {
-      setError(err.message);
-    }
-  };
-  
+export default function App() {
   return (
-    <div>
-      {!accessToken ? (
-        <form onSubmit={handleLogin}>
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit">Login</button>
-          {error && <p style={{ color: 'red' }}>{error}</p>}
-        </form>
-      ) : (
-
-        <div>
-          <h2>Rota Usercorp</h2>
-          <pre>{JSON.stringify(userData?.user, null, 2)}</pre>
-
-          <h3>Corporação</h3>
-          <ul>
-            {userData?.corporation?.slice(0, 10).map((corp) => (
-              <li key={corp.id}>{corp.name}</li>
-            ))}
-          </ul>
-
-          <h3>Dados</h3>
-          <ul>
-            {userData?.sites?.slice(0, 10).map((site) => (
-              <li key={site.id}>
-                {site.name} (Corp ID: {site.corporation})
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+    <main>
+      <Outlet />
+    </main>
   );
 }
-
-export default App;
