6fc85adfa533201ffc1ca77a007b734cff5d95dc
Wesley SantAnna <wesley.santanna@semeq.com>
2025-05-29 07:21:47 -0300

    Refatoração da minha main

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Login from './routes/Login.jsx';
+import Usercorp from './routes/usercorp.jsx';
+import Dashboard from './routes/Dashboard.jsx';
+import Lubricants from './routes/lubricants.jsx';
+import MobileTree from './routes/MobileTree.jsx';
+import MobileInfo from './routes/MobileInfo.jsx';
+import Static from './routes/Static.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Login /> },
+      { path: '/dashboard', element: <Dashboard /> },
+      { path: '/usercorp', element: <Usercorp /> },
+      { path: '/implantation/mobile/static/get_lubricants', element: <Lubricants/>},
+      { path: '/implantation/mobile/tree', element: <MobileTree /> },
+      { path: '/implantation/mobile/info', element: <MobileInfo /> },
+      { path: '/implantation/mobile/static', element: <Static />}
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
